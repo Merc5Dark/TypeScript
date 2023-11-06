e3082361ed2eb9b731ce49ef2a1543e1cb31f373
Merc5Dark <31131794siya@gmail.com>
2023-11-06 14:00:30 +0200

    Debugging

--- source/routes/test.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import controller from '../controllers/test';
-
-const router = express.Router();
-
-router.get('/testRoute', controller.testFunction0);
-
-export default router;

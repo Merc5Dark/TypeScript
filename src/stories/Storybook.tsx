f1f74ef674a404b4b6991e30cf569e7723e2a6d9
Merc5Dark <31131794siya@gmail.com>
2023-11-08 11:03:19 +0200

    testing

---
+++ src/stories/Storybook.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default {
+    title: 'Storybook Example',
+};
+
+export const SimpleStory = () => {
+    return (
+        <div>
+            <h1>Test</h1>
+            <p>Test</p>
+        </div>
+    )
+    
+};
